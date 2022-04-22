4de1db525c8a0f9975f6b8742e35257ea7cc7022
annatopalidi <topalidisanna@gmail.com>
2022-04-22 08:25:06 +0200

    refactoring: routes, change: gitignore, change path for hide

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :questions
-  patch '/questions/:id/hide', to: 'questions#hide'
+  resources :questions do
+    patch :hide, on: :member
+  end
   root to: 'questions#index'
 end
