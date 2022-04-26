98c4ecff9c04f17833c4a5e4dcff74a3472c7792
annatopalidi <topalidisanna@gmail.com>
2022-04-26 12:40:01 +0200

    change migrate create user

--- db/schema.rb
+++ db/schema.rb
@@ -23,10 +23,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_25_102641) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "nickname"
-    t.string "email"
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
