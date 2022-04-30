e1e8eca56dca08aa074c4d95abd6dccddfe6782c
annatopalidi <topalidisanna@gmail.com>
2022-04-30 08:38:03 +0200

    add markdown for question form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def inclination(count, one, few, many)
     many if (count % 100).between?(11, 14)
 
@@ -8,5 +9,27 @@ module ApplicationHelper
     else
       many
     end
+  end
+
+  # redcarpet markdown
+  def markdown(text)
+    options = {
+      filter_html: true,
+      hard_wrap: true,
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true,
+      fenced_code_blocks: true
+    }
+    extensions = {
+      autolink: true,
+      superscript: true,
+      disable_indented_code_blocks: true,
+      lax_spacing: true
+
+    }
+
+    renderer = ::Redcarpet::Render::HTML.new(options)
+    markdown = ::Redcarpet::Markdown.new(renderer, extensions)
+    markdown.render(text).html_safe
   end
 end
