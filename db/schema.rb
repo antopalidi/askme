f17b7567e36c9537ffbdab9345fbdab8eea7fa88
annatopalidi <topalidisanna@gmail.com>
2022-05-04 16:59:43 +0200

    add answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_03_133743) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_04_140000) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
     t.boolean "hidden", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "answer"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
