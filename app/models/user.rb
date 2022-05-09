4123eb89f712d9fb7c9d0539f1bd7740499476fb
annatopalidi <topalidisanna@gmail.com>
2022-05-09 21:31:54 +0200

    fix friendly_id (changed nickname)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
-  extend FriendlyId
+  include FriendlyId
 
   include Gravtastic
+
+  has_many :questions, dependent: :delete_all
 
   friendly_id :nickname, use: :slugged
 
   gravtastic(secure: true, filetype: :png, size: 100, default: 'wavatar')
-
-  has_many :questions, dependent: :delete_all
 
   has_secure_password
 
@@ -15,11 +15,15 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true, email: { mx_with_fallback: true }
   validates :nickname, presence: true, length: { maximum: 40 }, uniqueness: true, format: { with: /\A[a-z\d_]+\z/ }
-  validates :header_color, format: { with:/\A#\h{3}{1,2}\z/ }
+  validates :header_color, format: { with: /\A#\h{3}{1,2}\z/ }
 
   private
 
   def downcase_nickname
     nickname&.downcase!
+  end
+
+  def should_generate_new_friendly_id?
+    nickname_changed?
   end
 end
