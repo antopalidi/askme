120d4a8df8d5e2e542e7e70b3cd65f7087141997
annatopalidi <topalidisanna@gmail.com>
2022-05-09 12:19:50 +0200

    change validates header_color

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true, email: { mx_with_fallback: true }
   validates :nickname, presence: true, length: { maximum: 40 }, uniqueness: true, format: { with: /\A[a-z\d_]+\z/ }
-  validates :header_color, format: { with: /\A#(?:\h{3}){1,2}\z/ }
+  validates :header_color, format: { with:/\A#\h{3}{1,2}\z/ }
 
   private
 
